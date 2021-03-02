eb586f4fd28e53b2600c16a5b254373bcccc9d2a
James Go <jhoongo@gmail.com>
2021-03-01 17:14:36 -0800

    SDKS-912 :  Add FRAuth

--- Package.swift
+++ Package.swift
@@ -7,9 +7,11 @@ let package = Package (
         .iOS(.v10)
     ],
     products: [
-        .library(name: "FRCore", targets: ["FRCore"])
+        .library(name: "FRCore", targets: ["FRCore"]),
+        .library(name: "FRAuth", targets: ["FRAuth"])
     ],
     targets: [
-        .target(name: "FRCore", path: "FRCore/FRCore", exclude: ["Info.plist", "FRCore.h"])
+        .target(name: "FRCore", path: "FRCore/FRCore", exclude: ["Info.plist", "FRCore.h"]),
+        .target(name: "FRAuth", dependencies: ["FRCore"], path: "FRAuth/FRAuth", exclude: ["Info.plist", "FRAuth.h"])
     ]
 )
