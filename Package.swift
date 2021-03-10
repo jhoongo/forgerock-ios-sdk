b638478ed9423154925763a0a7f0747cb1bb4680
James Go <jhoongo@gmail.com>
2021-03-09 22:31:52 -0800

    SDKS-912 : Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -13,11 +13,11 @@ let package = Package (
         .library(name: "FRAuthenticator", targets: ["FRAuthenticator"])
     ],
     targets: [
-        .target(name: "cFRAuth", dependencies: [], path: "FRAuth/FRAuth/SharedC"),
-        .target(name: "cFRAuthenticator", dependencies: [], path: "FRAuthenticator/FRAuthenticator/SharedC"),
+        .target(name: "cFRAuth", dependencies: [], path: "FRAuth/FRAuth/SharedC/Sources"),
+        .target(name: "cFRAuthenticator", dependencies: [], path: "FRAuthenticator/FRAuthenticator/SharedC/Sources"),
         .target(name: "FRCore", path: "FRCore/FRCore", exclude: ["Info.plist", "FRCore.h"]),
         .target(name: "FRAuth", dependencies: [.target(name: "FRCore"), .target(name: "cFRAuth")], path: "FRAuth/FRAuth", exclude: ["Info.plist", "FRAuth.h", "SharedC/Sources/include/JBUtil.h", "SharedC/Sources/JBUtil.c", "SharedC/FRAuth.modulemap"]),
         .target(name: "FRProximity", dependencies: [.target(name: "FRAuth")], path: "FRProximity/FRProximity", exclude: ["Info.plist", "FRProximity.h"]),
-        .target(name: "FRAuthenticator", dependencies: [.target(name: "FRAuth"), .target(name: "cFRAuthenticator")], path: "FRAuthenticator/FRAuthenticator", exclude: ["Info.plist", "FRAuth.h", "SharedC/Sources/include/Base32.h", "SharedC/Sources/Base32.c", "SharedC/FRAuthenticator.modulemap"])
+        .target(name: "FRAuthenticator", dependencies: [.target(name: "FRAuth"), .target(name: "cFRAuthenticator")], path: "FRAuthenticator/FRAuthenticator", exclude: ["Info.plist", "FRAuthenticator.h", "SharedC/Sources/include/base32.h", "SharedC/Sources/base32.c", "SharedC/FRAuthenticator.modulemap"])
     ]
 )
